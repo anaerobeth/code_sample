fd49ca7f1a95e0bb59a938ef82e2fd8ab2546434
Beth Tenorio <betsky98@gmail.com>
2013-10-18 09:36:51 -0400

    Refactor Dna class

--- dna_statistics.rb
+++ dna_statistics.rb
@@ -1,18 +1,18 @@
+# A program to display the statistics related to a given DNA sequence
+# Author: Elizabeth Tenorio
+
 require 'pry'
 
 class Dna
 
-  attr_reader :sequence, :sorted_array, :statistics, :string
+  attr_reader :sequence
 
   def initialize(sequence)
     @sequence = sequence
-    @sorted_array = []
-    @statistics = []
-    @string = ''
   end
 
 
-  def count
+  def count_char
     hash = {}
     sequence.each_char do | char |
       hash[char] ? hash[char] << char : hash[char] = [char]
@@ -22,31 +22,31 @@ class Dna
       hash[key] = value.size.to_i
     end
 
-    @sorted_array = hash.sort
-    return @sorted_array
+    hash.sort
+  end
 
+  def total_char
+    sequence.length
   end
 
   def analyze
-    @total_bases = 0
-    count
-    @sorted_array.each do | item |
-      @total_bases += item[1]
+    statistics = []
+    count_char.each do | item |
+      statistics << [item[0], (item[1].to_f / total_char)]
     end
 
-    string << "DNA statistics (#{@total_bases} residue sequence)"
-    @sorted_array.each do | item |
-      @statistics << [item[0], (item[1].to_f / @total_bases)]
-    end
-    return @statistics
+    return statistics
   end
 
   def display
-    analyze
-    @statistics.each do | item |
-      string << " #{item[0].upcase}: #{(item[1]*100).round(1)}%"
+    output = ''
+    output << "DNA statistics (#{total_char} residue sequence)"
+
+    analyze.each do | item |
+      output << " #{item[0].upcase}: #{(item[1]*100).round(1)}%"
     end
-    return string
+
+    return output
   end
 end
 
--- dna_statistics_spec.rb
+++ dna_statistics_spec.rb
@@ -1,21 +1,27 @@
+# A program to display the statistics related to a given DNA sequence
+# Author: Elizabeth Tenorio
 require 'rspec'
 
-require_relative 'dna_stats'
+require_relative 'dna_statistics'
 
 describe 'Dna statistics' do
-  it 'counts the frequency of nucleotides in a string' do
-    expect(Dna.new('agccttaa').count).to eql([["a", 3], ["c", 2], ["g", 1], ["t", 2]])
+  it 'counts the total nucleotides in the string nggaatncc' do
+    expect(Dna.new('agccttaa').total_char).to eql(8)
   end
 
-  it 'counts the frequency of nucleotides in a string' do
-    expect(Dna.new('nggaatncc').count).to eql([["a", 2], ["c", 2], ["g", 2], ["n", 2], ["t", 1]])
+  it 'counts the frequency of nucleotides in the string agccttaa' do
+    expect(Dna.new('agccttaa').count_char).to eql([["a", 3], ["c", 2], ["g", 1], ["t", 2]])
   end
 
-  it 'analyzes the dna' do
+  it 'counts the frequency of nucleotides in the string nggaatncc' do
+    expect(Dna.new('nggaatncc').count_char).to eql([["a", 2], ["c", 2], ["g", 2], ["n", 2], ["t", 1]])
+  end
+
+  it 'analyzes the dna composition' do
     expect(Dna.new('aaggcccttt').analyze).to eql([["a", 0.2], ["c", 0.3], ["g", 0.2], ["t", 0.3]])
   end
 
-  it 'displays the dna statistics' do
+  it 'displays the percentage of nucleotides in the dna sequence' do
     expect(Dna.new('aaggcccttt').display).to eql("DNA statistics (10 residue sequence) A: 20.0% C: 30.0% G: 20.0% T: 30.0%")
   end
 end
