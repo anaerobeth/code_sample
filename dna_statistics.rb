ed063bd62846814cd067dc5a2c05eb12884a89fa
Beth Tenorio <betsky98@gmail.com>
2013-10-21 13:26:11 -0400

    Refactor academy_tracker

--- dna_statistics.rb
+++ dna_statistics.rb
@@ -11,7 +11,6 @@ class Dna
     @sequence = sequence
   end
 
-
   def count_char
     hash = {}
     sequence.each_char do | char |
@@ -21,25 +20,20 @@ class Dna
     hash.each do | key, value |
       hash[key] = value.size.to_i
     end
-
     hash.sort
   end
-
 
   def total_char
     sequence.length
   end
-
 
   def analyze
     statistics = []
     count_char.each do | item |
       statistics << [item[0], (item[1].to_f / total_char)]
     end
-
-    return statistics
+    statistics
   end
-
 
   def display
     output = ''
@@ -48,13 +42,10 @@ class Dna
     analyze.each do | item |
       output << " #{item[0].upcase}: #{(item[1]*100).round(1)}%"
     end
-
-    return output
+    output
   end
 end
 
 # Expected Output:
 # DNA statistics (52 residue sequence) A: 13.5% C: 34.6% G: 21.2% T: 30.8%
 puts Dna.new('aaggccctttacgtgcccttgccctttacgttacgtaccgggccctttacgt').display
-
-
